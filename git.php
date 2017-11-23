2822649f66cf93dc7576314bf61f03eaea34899a
zhangsison <1024760955@qq.com>
2017-11-23 15:55:32 +0800

    git 使用

---
+++ git.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+git init
+git add .
+git commit -m ""
+git remote add origin  https://github.com/zhangsison/chongdianbao
+git push -u origin master
+git clone
+git status
+
+缺少README.md
+git pull --rebase origin master
+
+删除文件
+git rm --cached filename
+git commit -m "hehe"
+git push origin
